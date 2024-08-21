376814c8cc29015b4625c25e9ed36ebf4c670dca
NexxyoLabsSergioCebrian <sergio.cebrian@nexxyolabs.com>
2024-08-21 17:40:23 +0200

    Updated signal and interface store

--- src/app/core/tasks/application/store/task-store.ts
+++ src/app/core/tasks/application/store/task-store.ts
@@ -6,32 +6,32 @@ import { TaskStoreInterface } from "@task:application/interfaces/task-store";
     providedIn: 'root'
 })
 export class TaskStore implements TaskStoreInterface {
-    tasks: WritableSignal<ITask[]> = signal<ITask[]>([]);
+    #tasks: WritableSignal<ITask[]> = signal<ITask[]>([]);
 
     addTaskAction(task: ITask): void {
-      this.tasks.update((tasks: ITask[]) => [...tasks, task]);
+      this.#tasks.update((tasks: ITask[]) => [...tasks, task]);
     }
 
     deleteTaskAction(id: string): void {
-      this.tasks.update((tasks: ITask[]) =>
+      this.#tasks.update((tasks: ITask[]) =>
         tasks.filter((taskItem: ITask) => taskItem.id !== id)
       );
     }
 
     loadTasksAction(): WritableSignal<ITask[]> {
-      return this.tasks;
+      return this.#tasks;
     }
 
     saveTaskAction(task: ITask): void {
-      this.tasks.update((tasks: ITask[]) => [...tasks, task]);
+      this.#tasks.update((tasks: ITask[]) => [...tasks, task]);
     }
 
     setTasksAction(tasks: ITask[]): void {
-      this.tasks.set(tasks);
+      this.#tasks.set(tasks);
     }
 
     updateTaskAction(task: ITask): void {
-      this.tasks.update((tasks: ITask[]) =>
+      this.#tasks.update((tasks: ITask[]) =>
         tasks.map((taskItem: ITask) => taskItem.id === task.id ? task : taskItem)
       );
     }
