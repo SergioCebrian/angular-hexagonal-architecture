7cf65f8681330907e40e7dc90d7a0b5ebb7b9465
NexxyoLabsSergioCebrian <sergio.cebrian@nexxyolabs.com>
2024-08-24 14:14:15 +0200

    Component tests

--- src/app/ui/components/task-create-form/task-create-form.component.spec.ts
+++ src/app/ui/components/task-create-form/task-create-form.component.spec.ts
@@ -1,21 +1,36 @@
-/* import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskCreateFormComponent } from './task-create-form.component';
-//import { ITask } from '@task:domain/models/task.model';
+import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { SaveTaskUseCase } from '@task:application/usecases/save-task-usecase/save-task-usecase';
+import { TaskStore } from '@task:application/store/task-store';
+import { ITask } from '@task:domain/models/task.model';
 
 describe('TaskCreateFormComponent', () => {
   let component: TaskCreateFormComponent;
   let fixture: ComponentFixture<TaskCreateFormComponent>;
-  //const mockTask: ITask = { id: '1', title: 'Task 1', isCompleted: false };
+  let saveTaskUseCaseSpy: jasmine.SpyObj<SaveTaskUseCase>;
+  let taskStoreSpy: jasmine.SpyObj<TaskStore>;
 
   beforeEach(async () => {
+    const saveTaskSpy = jasmine.createSpyObj('SaveTaskUseCase', ['saveTask']);
+    const storeSpy = jasmine.createSpyObj('TaskStore', ['saveTaskAction']);
+
     await TestBed.configureTestingModule({
-      imports: [TaskCreateFormComponent]
-    })
-    .compileComponents();
-    
+      declarations: [],
+      imports: [ReactiveFormsModule],
+      providers: [
+        { provide: SaveTaskUseCase, useValue: saveTaskSpy },
+        { provide: TaskStore, useValue: storeSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TaskCreateFormComponent);
     component = fixture.componentInstance;
+    saveTaskUseCaseSpy = TestBed.inject(
+      SaveTaskUseCase
+    ) as jasmine.SpyObj<SaveTaskUseCase>;
+    taskStoreSpy = TestBed.inject(TaskStore) as jasmine.SpyObj<TaskStore>;
+
     fixture.detectChanges();
   });
 
@@ -23,23 +38,48 @@ describe('TaskCreateFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('[Form: title] should create the form with a title control', () => {
+  it('[Form: title] should have a form with title control', () => {
     expect(component.createTaskForm.contains('title')).toBeTrue();
   });
 
-  it('[Form: validation] should validate the title control', () => {
-    const control = component.createTaskForm.get('title');
-    control?.setValue('');
-    expect(control?.valid).toBeFalse();
+  it('[Form: validate] should require title to be valid', () => {
+    const titleControl: AbstractControl<string | null, string | null> | null =
+    component.createTaskForm.get('title');
+    titleControl?.setValue('A');
+    expect(titleControl?.valid).toBeFalse();
+    titleControl?.setValue('Valid Title');
+    expect(titleControl?.valid).toBeTrue();
+  });
 
-    control?.setValue('abc');
-    expect(control?.valid).toBeFalse();
+  it('[saveTask] should call saveTaskUseCase.saveTask and taskStore.saveTaskAction when form is valid', async () => {
+    const newTask: ITask = {
+      id: '1',
+      title: 'New Task',
+      isCompleted: false,
+    };
 
-    control?.setValue('A valid task title');
-    expect(control?.valid).toBeTrue();
+    component.createTaskForm.setValue({ title: 'New Task' });
+    fixture.componentRef.setInput('total', 0);
 
-    control?.setValue('A'.repeat(31));
-    expect(control?.valid).toBeFalse(); 
+    saveTaskUseCaseSpy.saveTask.and.returnValue(Promise.resolve(newTask));
+
+    await component.saveTask();
+
+    expect(saveTaskUseCaseSpy.saveTask).toHaveBeenCalledWith({
+      id: '1',
+      title: 'New Task',
+      isCompleted: false,
+    });
+    expect(taskStoreSpy.saveTaskAction).toHaveBeenCalledWith(newTask);
+    expect(component.createTaskForm.value).toEqual({ title: null });
+  });
+
+  it('[saveTask] should not call saveTaskUseCase.saveTask or taskStore.saveTaskAction when form is invalid', async () => {
+    component.createTaskForm.setValue({ title: '' }); // Invalid form
+
+    await component.saveTask();
+
+    expect(saveTaskUseCaseSpy.saveTask).not.toHaveBeenCalled();
+    expect(taskStoreSpy.saveTaskAction).not.toHaveBeenCalled();
   });
 });
- */
--- src/app/ui/components/task-item/task-item.component.spec.ts
+++ src/app/ui/components/task-item/task-item.component.spec.ts
@@ -1,24 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TaskItemComponent } from './task-item.component';
-import { ITask } from '@task:domain/models/task.model';
 import { DebugElement } from '@angular/core';
+import { TaskItemComponent } from './task-item.component';
+import { TaskStore } from '@task:application/store/task-store';
+import { DeleteTaskUseCase } from '@task:application/usecases/delete-task-usecase/delete-task-usecase';
+import { UpdateTaskUseCase } from '@task:application/usecases/update-task-usecase/update-task-usecase';
+import { ITask } from '@task:domain/models/task.model';
 import { By } from '@angular/platform-browser';
-import { TaskRepository } from '@task:domain/repositories/task-repository';
 
 describe('TaskItemComponent', () => {
+  const mockTask: ITask = {
+    id: '1',
+    title: 'Test Task',
+    isCompleted: false,
+  };
   let component: TaskItemComponent;
   let debugElement: DebugElement;
   let fixture: ComponentFixture<TaskItemComponent>;
-  const mockTask: ITask = { id: '1', title: 'Task 1', isCompleted: false };
 
   beforeEach(async () => {
+    const deleteTaskSpy = jasmine.createSpyObj('DeleteTaskUseCase', [
+      'deleteTask',
+    ]);
+    const updateTaskSpy = jasmine.createSpyObj('UpdateTaskUseCase', [
+      'updateTask',
+    ]);
+    const storeSpy = jasmine.createSpyObj('TaskStore', [
+      'updateTaskAction',
+      'deleteTaskAction',
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [TaskItemComponent],
-      providers: [TaskRepository],
-    })
-    .compileComponents();
-    
+      declarations: [],
+      providers: [
+        { provide: DeleteTaskUseCase, useValue: deleteTaskSpy },
+        { provide: UpdateTaskUseCase, useValue: updateTaskSpy },
+        { provide: TaskStore, useValue: storeSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TaskItemComponent);
     component = fixture.componentInstance;
     debugElement = fixture.debugElement;
@@ -30,14 +49,16 @@ describe('TaskItemComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('[Property: title] should display the correct task title in the template', () => {
+    const taskNameElement = debugElement.query(By.css('.task-title'));
+    expect(taskNameElement?.nativeElement.textContent).toContain(
+      mockTask.title
+    );
+  });
+
   it('[onCheckboxChange] should update isCompleted property in task object when onCheckboxChange is called', () => {
     const event: Event = { target: { checked: true } } as unknown as Event;
     component.onCheckboxChange(event);
     expect(component.task().isCompleted).toBeTrue();
-  });
-
-  it('[Property: title] should display the correct task title in the template', () => {
-    const taskNameElement = debugElement.query(By.css('.task-title'));
-    expect(taskNameElement?.nativeElement.textContent).toContain(mockTask.title);
   });
 });
