338afd85acd4d24511caaa1fba1760f9fec9bf0e
NexxyoLabsSergioCebrian <sergio.cebrian@nexxyolabs.com>
2024-09-04 18:24:02 +0200

    Updated task store

--- src/app/core/contexts/tasks/infrastructure/store/task-store.ts
+++ src/app/core/contexts/tasks/infrastructure/store/task-store.ts
@@ -1,33 +1,36 @@
-import { Injectable, signal, WritableSignal, inject } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
 import { ITask } from '@task:domain/models/task.model';
-import { ITaskStore } from '@task:infra/store/task-store.interface';
-import { StoreService } from '@infra/store/store.service';
-import { IStore } from '@infra/store/store.interface';
+import { ITaskStore } from './task-store.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskStore implements ITaskStore {
-  #storeService: IStore<ITask> = inject(StoreService);
   #tasks: WritableSignal<ITask[]> = signal<ITask[]>([]);
 
   deleteTaskAction(id: string): void {
-    return this.#storeService.deleteAction(this.#tasks, id);
+    this.#tasks.update((tasks: ITask[]) =>
+      tasks.filter((taskItem: ITask) => taskItem.id !== id)
+    );
   }
 
   loadTasksAction(): WritableSignal<ITask[]> {
-    return this.#storeService.loadAction(this.#tasks);
+    return this.#tasks;
   }
 
   createTaskAction(task: ITask): void {
-    return this.#storeService.createAction(this.#tasks, task);
+    this.#tasks.update((tasks: ITask[]) => [...tasks, task]);
   }
 
   setTasksAction(tasks: ITask[]): void {
-    return this.#storeService.setAction(this.#tasks, tasks);
+    this.#tasks.set(tasks);
   }
 
   updateTaskAction(task: ITask): void {
-    return this.#storeService.updateAction(this.#tasks, task);
+    this.#tasks.update((tasks: ITask[]) =>
+      tasks.map((taskItem: ITask) =>
+        taskItem.id === task.id ? task : taskItem
+      )
+    );
   }
 }
--- src/app/core/infrastructure/store/store.interface.ts
+++
@@ -1,9 +0,0 @@
-import { WritableSignal } from "@angular/core";
-
-export interface IStore<T> {
-    deleteAction(prop: WritableSignal<T[]>, id: string): void;
-    loadAction(prop: WritableSignal<T[]>): WritableSignal<T[]>;
-    createAction(prop: WritableSignal<T[]>, item: T): void;
-    setAction(prop: WritableSignal<T[]>, items: T[]): void;
-    updateAction(prop: WritableSignal<T[]>, item: T): void;
-}
